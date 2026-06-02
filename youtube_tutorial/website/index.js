85763586f5c391a8d85fe6b8c14246c3148cc9d9
qslia <qsliang456@163.com>
2026-06-02 17:38:00 +0800

    if statements

--- youtube_tutorial/website/index.js
+++ youtube_tutorial/website/index.js
@@ -79,5 +79,81 @@
 //     document.getElementById("myH3").textContent = circumference + `cm`;
 // }
 
+// const decreseBtn = document.getElementById("descreaseBtn");
+// const resetBtn = document.getElementById("resetBtn");
+// const increaseBtn = document.getElementById("increaseBtn");
+// const countLabel = document.getElementById("countLabel");
+// let count = 0;
 
+// increaseBtn.onclick = function() {
+//     count++;
+//     countLabel.textContent = count;
+// }
+// decreaseBtn.onclick = function() {
+//     count--;
+//     countLabel.textContent = count;
+// }
+// resetBtn.onclick = function() {
+//     count = 0;
+//     countLabel.textContent = count;
+// }
+
+
+// console.log(Math.PI)
+// console.log(Math.E)
+
+// const min = 50;
+// const max = 100;
+
+// let randomNum = Math.floor(Math.random() * (max - min)) + min;
+// console.log(randomNum);
+
+// const myButton = document.getElementById("myButton");
+// const label1 = document.getElementById("label1");
+// const label2 = document.getElementById("label2");
+// const label3 = document.getElementById("label3");
+
+// const min = 1;
+// const max = 6;
+
+// let randomNum1;
+// let randomNum2;
+// let randomNum3;
+
+// myButton.onclick = function() {
+
+//     randomNum1 = Math.floor(Math.random() * (max - min)) + min;
+//     randomNum2 = Math.floor(Math.random() * (max - min)) + min;
+//     randomNum3 = Math.floor(Math.random() * (max - min)) + min;
+
+//     label1.textContent = randomNum1;
+//     label2.textContent = randomNum2;
+//     label3.textContent = randomNum3;
+// }
+
+// let age = 13;
+
+// if (age >= 18) {
+//     console.log(`You are an adult`);
+// } else if (age >= 13) {
+//     console.log(`You are a teenager`);
+// } else {
+//     console.log(`You are a child`);
+// }
+
+const myText = document.getElementById("myText");
+const mySubmit = document.getElementById("mySubmit");
+const resultElement = document.getElementById("resultElement");
+
+mySubmit.onclick = function() {
+    let age = myText.value;
+    age = Number(age);
+    if (age >= 18) {
+        resultElement.textContent = `You are an adult`;
+    } else if (age >= 13) {
+        resultElement.textContent = `You are a teenager`;
+    } else {
+        resultElement.textContent = `You are a child`;
+    }
+}
 
