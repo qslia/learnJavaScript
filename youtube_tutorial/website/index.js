250599c0653dd5aa469e517b84a158c1d9b61d42
qslia <qsliang456@163.com>
2026-06-07 09:58:35 +0800

    temperature conversion

--- youtube_tutorial/website/index.js
+++ youtube_tutorial/website/index.js
@@ -1,185 +1,44 @@
-// let age = 21;
-// let message = age >= 18 ? "You are an adult" : "You are a minor";
-// console.log(message);
-
-// let purchaseAmount = 125;
-// let discount = purchaseAmount >= 100 ? 10 : 0;
-// let finalAmount = purchaseAmount - discount;
-// console.log(finalAmount);
-
-// let day = 1;
-
-// switch(day) {
-//     case 1:
-//     case 2:
-//         console.log("Tuesday");
-//         break;
-//     case 3:
-//         console.log("Wednesday");
-//         break;
-//     case 4:
-//         console.log("Thursday");
-//         break;
-//     case 5:
-//         console.log("Friday");
-//         break;
-//     case 6:
-//         console.log("Saturday");
-//         break;
-//     case 7:
-//         console.log("Sunday");
-//         break;
-//     default:
-//         console.log("Invalid day");
-//         break;
+// function happyBirthday() {
+//     console.log("Happy Birthday to you!");
+//     console.log("Happy Birthday to you!");
+//     console.log("Happy Birthday dear you!");
+//     console.log("Happy Birthday to you!");
 // }
 
-// let testScore = 92;
-// let letterGrade;
+// happyBirthday();
+// function1()
 
-// switch(true) {
-//     case testScore >= 90:
-//         letterGrade = "A";
-//         break;
-//     case testScore >= 80:
-//         letterGrade = "B";
-//         break;
-//     case testScore >= 70:
-//         letterGrade = "C";
-//         break;
-//     case testScore >= 60:
-//         letterGrade = "D";
-//         break;
-//     default:
-//         letterGrade = "F";
-//         break;
-// }
-// console.log(letterGrade);
-
-// let userName = "BroCode  ";
-// userName.charAt(0);
-// console.log(userName.charAt(0));
-// console.log(userName.charAt(1));
-// console.log(userName.indexOf("o"));
-// console.log(userName.length);
-// console.log(userName.trim());
-// console.log(userName.toUpperCase());
-// console.log(userName.toLowerCase());
-// console.log(userName.includes("Code"));
-// console.log(userName.includes("code"));
-// console.log(userName.startsWith("Bro"));
-// console.log(userName.endsWith("Code"));
-
-// let phoneNumber = "123-456-7890";
-// console.log(phoneNumber.split("-"));
-// console.log(phoneNumber.replaceAll("-", "/"));
-// console.log(phoneNumber.padStart(15, "0"));
-
-// const fullName = "Bro Code";
-// const firstName = fullName.slice(0, 3);
-// console.log(firstName);
-// const lastName = fullName.slice(4);
-// console.log(lastName);
-// const fullName2 = firstName + " " + lastName;
-// console.log(fullName2);
-// const fullName3 = fullName.replace("Bro", "John");
-// console.log(fullName3);
-// const fullName4 = fullName.replaceAll("o", "a");
-// console.log(fullName4);
-
-// const email = "Brol@gmail.com";
-// console.log(email.substring(0, 3));
-// console.log(email.slice(0, email.indexOf("@")));
-// console.log(email.slice(email.indexOf("@") + 1));
-
-// let userName = window.prompt("Enter your name");
-// userName = userName.trim();
-// let letter = userName.charAt(0);
-// letter = letter.toUpperCase();
-
-// let extraChars = userName.slice(1).toLowerCase();
-// userName = letter + extraChars;
-// console.log(userName);
-
-
-// const temp = 20;
-
-// if(temp > 0 && temp < 30) {
-//     console.log("The weather is good");
-// } else if(temp >= 30 && temp < 40) {
-//     console.log("The weather is hot");
-// } else if(temp >= 40 && temp < 50) {
-//     console.log("The weather is very hot");
-// } else {
-//     console.log("The weather is cold");
+// function function1() {
+//     let x = 1;
+//     console.log(x);
 // }
 
-// const PI = 3.14;
-
-// if (PI === "3.14") {
-//     console.log("PI is 3.14");
-// } else {
-//     console.log("PI is not 3.14");
+// function function2() {
+//     let y = 3;
+//     console.log(x);
 // }
 
-// let userName = "";
-// while (userName === "" || userName === null) {
-//     userName = window.prompt("Enter your name");
-// }
-// console.log("Hello", userName);
+const textBox = document.getElementById("textBox");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const toCelsius = document.getElementById("toCelsius");
+const result = document.getElementById("result");
+const myButton = document.getElementById("myButton");
 
+myButton.addEventListener("click", convert);
 
-// let userName = "";
-// do {
-//     userName = window.prompt("Enter your name");
-// } while (userName === "" || userName === null) 
-// console.log("Hello", userName);
-
-// let loggedIn = false;
-// let username;
-// let password;
-
-// while (!loggedIn) {
-//     username = window.prompt("Enter your username");
-//     password = window.prompt("Enter your password");
-//     if (username === "admin" && password === "123") {
-//         loggedIn = true;
-//     } else {
-//         console.log("Invalid username or password");
-//     }
-// }
-// console.log("Logged in successfully");
-
-// for (let i = 1; i <= 10; i += 2) {
-//     console.log(i);
-// }
-// console.log("Happy New Year");
-
-const minNum = 1;
-const maxNum = 100;
-const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum
-
-let attempts = 0;
-let guess;
-let running = true;
-
-console.log(answer);
-while (running) {
-    guess = window.prompt("Enter your guess");
-    if (Number(guess) === answer) {
-        console.log("You guessed it");
-        running = false;
+function convert() {
+    const temperature = parseFloat(textBox.value);
+    if (isNaN(temperature)) {
+        result.textContent = "Please enter a valid number";
+        return;
     }
-    attempts++;
-    if (guess < answer) {
-        window.alert("Too low");
-    } else if (guess > answer) {
-        window.alert("Too high");
+    if (toFahrenheit.checked) {
+        const fahrenheit = (temperature * 9/5) + 32;
+        result.textContent = `${temperature}°C = ${fahrenheit.toFixed(2)}°F`;
+    } else if (toCelsius.checked) {
+        const celsius = (temperature - 32) * 5/9;
+        result.textContent = `${temperature}°F = ${celsius.toFixed(2)}°C`;
     } else {
-        window.alert("Invalid guess");
+        result.textContent = "Select a unit";
     }
 }
-console.log(`You guessed it in ${attempts} attempts`);
-
-
-
