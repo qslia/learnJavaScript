1e158eea0144ada0abf5775428c13d1bf2ae51d1
qslia <qsliang456@163.com>
2026-06-21 19:10:10 +0800

    arrow functions

--- youtube_tutorial/website/index.js
+++ youtube_tutorial/website/index.js
@@ -1,34 +1,39 @@
 // function hello() {
-//     console.log("hello");
+//     console.log("Hello");
 // }
+// hello();
 
 // const hello = function() {
-//     console.log("hello");
+//     console.log("hello")
 // }
 
 // hello();
 
-// setTimeout(function() {
-//     console.log("hello");
-// }, 3000);
 
-const numbers = [1, 2, 3, 4, 5, 6];
-const squares = numbers.map(function(element) {
-    return Math.pow(element, 2);
-});
+// const hello = () => console.log("hello")
 
-const evenNums = numbers.filter(function(element) {
-    return element % 2 === 0;
-})
+// hello();
 
-const total = numbers.reduce(function(accumulator, element) {
-    return accumulator + element;
-})
 
+// const hello = (name) => console.log(`hello ${name}`)
+
+// hello("bro");
+
+// const hello = (name, age) => {
+//     console.log(`hello ${name}`)
+//     console.log(`You are ${age} years old`);
+// }
+
+// hello("bro", 25);
+
+// setTimeout(() => console.log("hello"), 3000);
+
+const numbers = [1, 2, 3, 4, 5, 6]
+const squares = numbers.map((element) => Math.pow(element, 2));
+const cubes = numbers.map((element) => Math.pow(element, 3));
+const evenNums = numbers.filter((element) => element % 2 === 0);
+const total = numbers.reduce((accumulator, element) => accumulator + element);
+console.log(total);
+console.log(cubes);
 console.log(squares);
 console.log(evenNums);
-console.log(total);
-
-
-
-
