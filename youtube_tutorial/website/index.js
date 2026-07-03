1eb2831797a85657b0be02089f7dbf8ab64e6568
qslia <qsliang456@163.com>
2026-07-03 08:52:21 +0800

    constructor

--- youtube_tutorial/website/index.js
+++ youtube_tutorial/website/index.js
@@ -1,39 +1,35 @@
-const person1 = {
-    firstName: "John",
-    lastName: "Doe",
-    age: 30,
-    city: "New York",
-    country: "USA",
-    email: "john.doe@example.com",
-    phone: "+1234567890",
-    address: "123 Main St, Anytown, USA",
-    zip: "12345",
-    state: "NY",
-    country: "USA",
-    isEmployee: true,
-    sayHello: function() {
-        console.log("Hello, my name is " + this.firstName + " " + this.lastName);
+function Car(make, model, year, color) {
+    this.make = "Ford";
+    this.model = "Mustang";
+    this.year = 2020;
+    this.color = "Red";
+    this.speed = 0;
+    this.accelerate = function() {
+        this.speed += 10;
+        console.log("The car is accelerating to " + this.speed + " mph");
+    }
+    this.brake = function() {
+        this.speed -= 10;
+        console.log("The car is braking to " + this.speed + " mph");
     }
 }
-const person2 = {
-    firstName: "Jane",
-    lastName: "Doe",
-    age: 25,
-    city: "Los Angeles",
-    country: "USA",
-    email: "jane.doe@example.com",
-    phone: "+1234567890",
-    address: "123 Main St, Anytown, USA",
-    zip: "12345",
-    state: "CA",
-    country: "USA",
-    isEmployee: false,
-    sayHello: () => {
-        console.log("Hello, my name is " + this.firstName + " " + this.lastName);
-    } // Arrow functions don't have their own this binding
-}
 
-console.log(person1.firstName);
-console.log(person2.firstName);
-console.log(person1.sayHello());
-console.log(person2.sayHello());
+const car1 = new Car("Ford", "Mustang", 2020, "Red");
+console.log(car1);
+console.log(car1.make);
+console.log(car1.model);
+console.log(car1.year);
+console.log(car1.color);
+console.log(car1.speed);
+car1.accelerate();
+car1.brake();
+
+const car2 = new Car("Chevy", "Camaro", 2021, "Blue");
+console.log(car2);
+console.log(car2.make);
+console.log(car2.model);
+console.log(car2.year);
+console.log(car2.color);
+console.log(car2.speed);
+car2.accelerate();
+car2.brake();
