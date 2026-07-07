09652337ae4d9bcd7396d822be15ada4e6fda5cc
qslia <qsliang456@163.com>
2026-07-07 10:23:00 +0800

    static method

--- youtube_tutorial/website/index.js
+++ youtube_tutorial/website/index.js
@@ -1,25 +1,49 @@
-class Product {
-    constructor(name, price) {
+// class MathUtil {
+//     static PI = 3.14159;
+    
+//     static getDiameter(radius) {
+//         return radius * 2;
+//     }
+
+//     static getCircumference(radius) {
+//         return 2 * MathUtil.PI * radius;
+//     }
+
+//     static getArea(radius) {
+//         return MathUtil.PI * radius * radius;
+//     }
+// }
+
+// console.log(MathUtil.PI);
+// console.log(MathUtil.getDiameter(10));
+// console.log(MathUtil.getCircumference(10));
+// console.log(MathUtil.getArea(10));
+
+class User {
+    static userCount = 0;
+    constructor(name) {
         this.name = name;
-        this.price = price;
+        User.userCount++;
     }
-
-    displayProduct() {
-        console.log(`${this.name} - ${this.price.toFixed(2)}`);
+    static getUserCount() {
+        console.log(`There are ${User.userCount} users online`)
     }
-
-    calculateTotal(salesTax) {
-        return this.price + (this.price * salesTax);
+    sayHello() {
+        console.log(`Hello, my name is ${this.name}`);
     }
 }
 
-const product1 = new Product('Product 1', 100.223);
-const product2 = new Product('Product 2', 200.33);
-product1.displayProduct();
-product2.displayProduct();
-
-const salesTax = 0.1;
-const total1 = product1.calculateTotal(salesTax);
-const total2 = product2.calculateTotal(salesTax);
-console.log(`Total 1: ${total1.toFixed(2)}`);
-console.log(`Total 2: ${total2.toFixed(2)}`);
+const user1 = new User('John');
+const user2 = new User('Jane');
+const user3 = new User('Jim');
+console.log(user1.name);
+console.log(user2.name);
+console.log(user3.name);
+console.log(user1.userCount);
+console.log(user2.userCount);
+console.log(user3.userCount);
+console.log(User.userCount);
+user1.sayHello();
+user2.sayHello();
+user3.sayHello();
+User.getUserCount()
