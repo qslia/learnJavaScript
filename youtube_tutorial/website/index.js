0cb2a9b03d0a6180ae7247dd4cf3949f8404eb87
qslia <qsliang456@163.com>
2026-07-07 10:10:21 +0800

    class

--- youtube_tutorial/website/index.js
+++ youtube_tutorial/website/index.js
@@ -1,35 +1,25 @@
-function Car(make, model, year, color) {
-    this.make = "Ford";
-    this.model = "Mustang";
-    this.year = 2020;
-    this.color = "Red";
-    this.speed = 0;
-    this.accelerate = function() {
-        this.speed += 10;
-        console.log("The car is accelerating to " + this.speed + " mph");
+class Product {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
     }
-    this.brake = function() {
-        this.speed -= 10;
-        console.log("The car is braking to " + this.speed + " mph");
+
+    displayProduct() {
+        console.log(`${this.name} - ${this.price.toFixed(2)}`);
+    }
+
+    calculateTotal(salesTax) {
+        return this.price + (this.price * salesTax);
     }
 }
 
-const car1 = new Car("Ford", "Mustang", 2020, "Red");
-console.log(car1);
-console.log(car1.make);
-console.log(car1.model);
-console.log(car1.year);
-console.log(car1.color);
-console.log(car1.speed);
-car1.accelerate();
-car1.brake();
+const product1 = new Product('Product 1', 100.223);
+const product2 = new Product('Product 2', 200.33);
+product1.displayProduct();
+product2.displayProduct();
 
-const car2 = new Car("Chevy", "Camaro", 2021, "Blue");
-console.log(car2);
-console.log(car2.make);
-console.log(car2.model);
-console.log(car2.year);
-console.log(car2.color);
-console.log(car2.speed);
-car2.accelerate();
-car2.brake();
+const salesTax = 0.1;
+const total1 = product1.calculateTotal(salesTax);
+const total2 = product2.calculateTotal(salesTax);
+console.log(`Total 1: ${total1.toFixed(2)}`);
+console.log(`Total 2: ${total2.toFixed(2)}`);
