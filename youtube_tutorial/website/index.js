25449d4713e071b8a7d85752ea736d6e68dba5da
qslia <qsliang456@163.com>
2026-07-07 10:31:09 +0800

    inheritance

--- youtube_tutorial/website/index.js
+++ youtube_tutorial/website/index.js
@@ -1,49 +1,40 @@
-// class MathUtil {
-//     static PI = 3.14159;
-    
-//     static getDiameter(radius) {
-//         return radius * 2;
-//     }
+class Animal {
+    alive = true;
 
-//     static getCircumference(radius) {
-//         return 2 * MathUtil.PI * radius;
-//     }
-
-//     static getArea(radius) {
-//         return MathUtil.PI * radius * radius;
-//     }
-// }
-
-// console.log(MathUtil.PI);
-// console.log(MathUtil.getDiameter(10));
-// console.log(MathUtil.getCircumference(10));
-// console.log(MathUtil.getArea(10));
-
-class User {
-    static userCount = 0;
-    constructor(name) {
-        this.name = name;
-        User.userCount++;
+    eat() {
+        console.log(`This ${this.name} is eating`);
     }
-    static getUserCount() {
-        console.log(`There are ${User.userCount} users online`)
-    }
-    sayHello() {
-        console.log(`Hello, my name is ${this.name}`);
+    sleep() {
+        console.log(`This ${this.name} is sleeping`);
     }
 }
 
-const user1 = new User('John');
-const user2 = new User('Jane');
-const user3 = new User('Jim');
-console.log(user1.name);
-console.log(user2.name);
-console.log(user3.name);
-console.log(user1.userCount);
-console.log(user2.userCount);
-console.log(user3.userCount);
-console.log(User.userCount);
-user1.sayHello();
-user2.sayHello();
-user3.sayHello();
-User.getUserCount()
+class Rabbit extends Animal {
+    name = "rabbit";
+    run() {
+        console.log(`This ${this.name} is running`);
+    }
+
+}
+class Fish extends Animal {
+    name = "fish";
+    swim() {
+        console.log(`This ${this.name} is swimming`);
+    }
+}
+
+class Hawk extends Animal {
+    name = "hawk";
+    fly() {
+        console.log(`This ${this.name} is flying`);
+    }
+}
+
+const rabbit = new Rabbit()
+const fish = new Fish()
+const hawk = new Hawk()
+
+console.log(hawk.alive);
+hawk.eat()
+hawk.sleep()
+hawk.fly()
