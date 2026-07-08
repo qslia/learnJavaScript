c4fc652cb1c04928f606db3565160b6f493ca091
qslia <qsliang456@163.com>
2026-07-08 20:38:15 +0800

    getters and setters

--- youtube_tutorial/website/index.js
+++ youtube_tutorial/website/index.js
@@ -1,55 +1,81 @@
-class Animal {
-    constructor(name, age) {
-        this.name = name;
+// class Rectangle {
+
+//     constructor(width, height) {
+//         this.width = width;
+//         this.height = height;
+//     }
+
+//     set width(newWidth) {
+//         if (newWidth > 0) {
+//             this._width = newWidth;
+//         } else {
+//             console.error("Width must be a positive number.")
+//         }
+//     }
+
+//     set height(newHeight) {
+//         if (newHeight > 0) {
+//             this._height = newHeight;
+//         } else {
+//             console.error("Height must be a positive number.")
+//         }
+//     }
+
+//      get Width() {
+//         return `${this._width.toFixed(1)} cm`;
+//      }
+
+//      get Height() {
+//         return `${this._height} cm`;
+//      }
+
+//      get area() {
+//         return `${(this._width * this._height).toFixed(1)} cm^2`;
+//      }
+// }
+
+// const rectangle = new Rectangle(3, 4)
+
+// rectangle.width = -100;
+// rectangle.height = "pizza";
+
+// console.log(rectangle.Width);
+// console.log(rectangle.Height);
+// console.log(rectangle.area)
+
+
+class Person {
+    constructor(firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.age = age;
     }
+    set firstName(newFirstName) {
+        if (typeof newFirstName === "string" && newFirstName.length > 0) {
+            this._firstName = newFirstName;
+        } else {
+            console.error("First name must be a non-empty string");
+        }
+    }
 
-    move(speed) {
-        console.log(`The ${this.name} moves at a speed of ${speed} mph.`);
+    set age(newAge) {
+        if (typeof newAge === "number" && newAge >= 0) {
+            this._age = newAge;
+        } else {
+            console.error("Age must be a non-negative number");
+        }
+    }
+    get firstName() {
+        return this._firstName;
+    }
+
+    get age() {
+        return this._age;
     }
 }
 
-class Rabbit extends Animal {
-    constructor(name, age, runSpeed) {
-        super(name, age);
-        this.runSpeed = runSpeed;
-    }
-    run() {
-        console.log(`This ${this.name} can run`);
-        super.move(this.runSpeed)
-    }
-}
-class Fish extends Animal {
-    constructor(name, age, swimSpeed) {
-        super(name, age);
-        this.swimSpeed = swimSpeed;
-    }
-    swim() {
-        console.log(`This ${this.name} can swim`);
-        super.move(this.swimSpeed)
-    }
-}
+const person = new Person('barry', "allen", 12);
 
-class Hawk extends Animal {
-    constructor(name, age, flySpeed) {
-        super(name, age);
-        this.flySpeed = flySpeed;
-    }
-
-    fly() {
-        console.log(`This ${this.name} can fly`);
-        super.move(this.flySpeed)
-    }
-
-}
-
-const rabbit = new Rabbit("rabbit", 1, 25);
-const fish = new Fish("fish", 2, 12);
-const hawk = new Hawk("hawk", 3, 50);
-
-console.log(rabbit.name);
-console.log(rabbit.age)
-console.log(rabbit.runSpeed)
-rabbit.run();
-fish.swim();
-hawk.fly();
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.age);
