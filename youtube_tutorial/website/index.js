0e54fc270996ade33812d0acc4462fbaa9afd964
qslia <qsliang456@163.com>
2026-07-16 18:37:14 +0800

    shuffle

--- youtube_tutorial/website/index.js
+++ youtube_tutorial/website/index.js
@@ -1,26 +1,13 @@
-const fruits = [
-    {name: "apple", color: "red", calories: 95},
-    {name: "apple1", color: "red1", calories: 96},
-    {name: "apple2", color: "red2", calories: 97},
-    {name: "apple3", color: "red3", calories: 98},
-]
+const cards = ['A', 2, 3, 4, 5, 'J', 'Q', 'K'];
+// cards.sort(() => Math.random() - 0.5);
 
+shuffle(cards);
+console.log(cards);
 
-fruits.push({name: "apple4", color: "red4", calories: 99})
-// fruits.pop()
-// fruits.splice(1, 3)
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const random = Math.floor(Math.random() * (i + 1));
+        [array[i], array[random]] = [array[random], array[i]];
+    }
+}
 
-// fruits.forEach(fruit => console.log(fruit.calories));
-// const fruitNames = fruits.map(fruit => fruit.name)
-// const yellowFruits = fruits.filter(fruit => fruit.color == 'red2')
-// const lowCalFruits = fruits.filter(fruit => fruit.calories < 100);
-// const highCalFruits = fruits.filter(fruit => fruit.calories > 100);
-
-const maxFruit = fruits.reduce((max, fruit) => 
-                                fruit.calories > max.calories ?
-                                fruit: max);
-const minFruit = fruits.reduce((min, fruit) => 
-    fruit.calories < min.calories ?
-    fruit: min);
-
-console.log(minFruit)
