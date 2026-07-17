9b9b77fe0dbc00b024e170c1c4191a7aa7f3c053
qslia <qsliang456@163.com>
2026-07-17 19:41:52 +0800

    interest calculator

--- youtube_tutorial/website/index.js
+++ youtube_tutorial/website/index.js
@@ -1,16 +1,32 @@
-// let number = 123456.789;
-// number = number.toLocaleString(undefined);
+function calculate() {
+    const totalAmount = document.getElementById("total-amount");
+    const principalInput = document.getElementById("principal");
+    const rateInput = document.getElementById("rate");
+    const yearsInput = document.getElementById("years");
 
-// console.log(number);
+    let principal = Number(principalInput.value);
+    let rate = Number(rateInput.value / 100);
+    let years = Number(yearsInput.value);
 
-let number = 123456.789;
-// number = number.toLocaleString("en-US");
-// number = number.toLocaleString("hi-IN");
-// number = number.toLocaleString("de-DE");
+    if (principal < 0 || isNaN(principal)) {
+        principal = 0;
+        principalInput.value = 0;
+    }
+    if (rate < 0 || isNaN(rate)) {
+        rate = 0;
+        rateInput.value = 0;
+    }
+    if (years < 0 || isNaN(years)) {
+        years = 0;
+        yearsInput.value = 0;
+    }
 
-// number = number.toLocaleString("en-US", {style: "currency", currency: "USD"});
-// number = number.toLocaleString("hi-IN", {style: "currency", currency: "INR"});
-number = number.toLocaleString("de-DE", {style: "currency", currency: "EUR"});
-console.log(number);
+    const result = principal * Math.pow((1 + rate / 1), 1 * years);
+
+    totalAmount.textContent = result.toLocaleString("en-US", {style: "currency", currency: "USD"});
+
+// undefined 表示使用系统默认区域设置（通常是浏览器的语言环境）
+// 如果你的浏览器语言不是英语（美国），系统不确定该显示 $ 还是 USD，于是同时显示两者作为后备方案
+}
 
 
